e04147bf6988ba973d15c981c9eb7ce29c99464b
VAHE <vahemikayelyan@gmail.com>
2016-11-29 13:59:39 +0400

    Added WebAppConfig.java

---
+++ src/main/java/com/springapp/mvc/configuration/WebAppConfig.java
@@ -0,0 +1,18 @@
+package com.springapp.mvc.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
+}
