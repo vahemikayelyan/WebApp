256f417cd15cd7222c94ed535c4922b36d4754bc
VAHE <vahemikayelyan@gmail.com>
2016-12-05 17:23:05 +0400

    data sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+/* Populate USER_PROFILE Table */
+INSERT INTO USER_PROFILE(type)
+VALUES ('USER');
+
+INSERT INTO USER_PROFILE(type)
+VALUES ('ADMIN');
+
+INSERT INTO USER_PROFILE(type)
+VALUES ('DBA');
+
+
+/* password = abc125 */
+/* Populate one Admin User which will further create other users for the application using GUI */
+INSERT INTO APP_USER(sso_id, password, first_name, last_name, email)
+VALUES ('sam','$2a$10$4eqIF5s/ewJwHK1p8lqlFOEm2QIA0S8g6./Lok.pQxqcxaBZYChRm', 'Sam','Smith','samy@xyz.com');
+
+
+/* Populate JOIN Table */
+INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
+  SELECT user.id, profile.id FROM app_user user, user_profile profile
+  where user.sso_id='sam' and profile.type='ADMIN';
