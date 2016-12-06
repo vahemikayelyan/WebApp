005f5f11eb47e8e39763f96c6ecd2870dbc5ba07
VAHE <vahemikayelyan@gmail.com>
2016-12-06 17:43:09 +0400

    added security configuration

--- src/main/java/com/springapp/mvc/controller/AppController.java
+++ src/main/java/com/springapp/mvc/controller/AppController.java
@@ -1,37 +1,26 @@
 package com.springapp.mvc.controller;
 
-import java.util.List;
-import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-
 import com.springapp.mvc.model.User;
 import com.springapp.mvc.model.UserProfile;
 import com.springapp.mvc.service.UserProfileService;
 import com.springapp.mvc.service.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.security.authentication.AuthenticationTrustResolver;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Locale;
 
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/backend")
 @SessionAttributes("roles")
 public class AppController {
 
@@ -44,22 +33,15 @@ public class AppController {
     @Autowired
     MessageSource messageSource;
 
-    @Autowired
-    PersistentTokenBasedRememberMeServices persistentTokenBasedRememberMeServices;
-
-    @Autowired
-    AuthenticationTrustResolver authenticationTrustResolver;
-
-
     /**
      * This method will list all existing users.
      */
-    @RequestMapping(value = {"/", "/list"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"", "/", "/list"}, method = RequestMethod.GET)
     public String listUsers(ModelMap model) {
         List<User> users = userService.findAllUsers();
         model.addAttribute("users", users);
         model.addAttribute("loggedinuser", getPrincipal());
-        return "userslist";
+        return "usersList";
     }
 
     /**
@@ -102,8 +84,7 @@ public class AppController {
 
         model.addAttribute("success", "User " + user.getFirstName() + " " + user.getLastName() + " registered successfully");
         model.addAttribute("loggedinuser", getPrincipal());
-        //return "success";
-        return "registrationsuccess";
+        return "registrationSuccess";
     }
 
 
@@ -124,8 +105,7 @@ public class AppController {
      * updating user in database. It also validates the user input
      */
     @RequestMapping(value = {"/edit-user-{ssoId}"}, method = RequestMethod.POST)
-    public String updateUser(@Valid User user, BindingResult result,
-                             ModelMap model, @PathVariable String ssoId) {
+    public String updateUser(@Valid User user, BindingResult result, ModelMap model, @PathVariable String ssoId) {
 
         if (result.hasErrors()) {
             return "registration";
@@ -143,7 +123,7 @@ public class AppController {
 
         model.addAttribute("success", "User " + user.getFirstName() + " " + user.getLastName() + " updated successfully");
         model.addAttribute("loggedinuser", getPrincipal());
-        return "registrationsuccess";
+        return "registrationSuccess";
     }
 
 
@@ -175,34 +155,6 @@ public class AppController {
     }
 
     /**
-     * This method handles login GET requests.
-     * If users is already logged-in and tries to goto login page again, will be redirected to list page.
-     */
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginPage() {
-        if (isCurrentAuthenticationAnonymous()) {
-            return "login";
-        } else {
-            return "redirect:/list";
-        }
-    }
-
-    /**
-     * This method handles logout requests.
-     * Toggle the handlers if you are RememberMe functionality is useless in your app.
-     */
-    @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            //new SecurityContextLogoutHandler().logout(request, response, auth);
-            persistentTokenBasedRememberMeServices.logout(request, response, auth);
-            SecurityContextHolder.getContext().setAuthentication(null);
-        }
-        return "redirect:/login?logout";
-    }
-
-    /**
      * This method returns the principal[user-name] of logged-in user.
      */
     private String getPrincipal() {
@@ -215,13 +167,5 @@ public class AppController {
             userName = principal.toString();
         }
         return userName;
-    }
-
-    /**
-     * This method returns true if users is already authenticated [logged-in], else false.
-     */
-    private boolean isCurrentAuthenticationAnonymous() {
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authenticationTrustResolver.isAnonymous(authentication);
     }
 }
